e3b6cde009dc72dbf666e925014292902e383653
kartiksharma548 <kartiksharma548@gmail.com>
2021-11-28 20:56:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+
+const express=require('express')
+
+const jwt=require('jsonwebtoken')
+const tokenMiddleware=require('./utils/middlewareAuth')
+const http=require('http');
+
+var app=express();
+var server=http.Server(app)
+
+
+
+app.use(express.json());
+
+
+app.get('/',(req,res)=>{
+    res.send("Page Visited");
+})
+
+app.post('/login',(req,res)=>{
+
+    //Authenticate Id/Pwd from Database
+    
+const userObj={userid:req.body.name};
+const token=jwt.sign(userObj,process.env.ACCESS_TOKEN,{
+    expiresIn: "25s",
+  })
+
+res.json({accesstoken:token})   
+
+})
+
+app.post('/register',(req,res)=>{
+
+    //Authenticate Id/Pwd from Database
+    
+const userObj={userid:req.body.name};
+const token=jwt.sign(userObj,process.env.ACCESS_TOKEN)
+
+res.json({accesstoken:token})   
+
+})
+
+app.get('/pageOne',tokenMiddleware,(req,res)=>{
+    res.send("Page Visited");
+})
+
+server.listen(3000,()=>{
+console.log('Server Started')
+});
+
+
