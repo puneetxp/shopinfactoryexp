98d14247e0c95e61e60a228b216822f83d3be334
kartiksharma548 <kartiksharma548@gmail.com>
2021-12-03 00:12:44 +0530

    Added Login

--- index.js
+++ index.js
@@ -20,16 +20,21 @@ app.get('/', (req, res) => {
     res.send("Page Visited");
 })
 
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
 
     //Authenticate Id/Pwd from Database
+    console.log(req.body.pwd)
+    const userFound = await db.db.collection('userInfo').findOne({ email: req.body.email, pwd: req.body.pwd })
+
+console.log(userFound)
+    if (userFound == null) return res.send({ msg: "Email/Pwd is not correct.", status: 'F' })
 
     const userObj = { userid: req.body.name };
     const token = jwt.sign(userObj, process.env.ACCESS_TOKEN, {
         expiresIn: "30m",
     })
 
-    res.json({ accesstoken: token })
+    res.json({ accesstoken: token, status: 'S' })
 
 })
 
@@ -37,11 +42,11 @@ app.post('/register', (req, res) => {
 
     //Authenticate Id/Pwd from Database
     console.log(req.body);
-    const userObj = { firstName: req.body.firstname, lastName: req.body.lastname, Email: req.body.email, pwd: req.body.password }
+    const userObj = { firstName: req.body.firstname, lastName: req.body.lastname, email: req.body.email, pwd: req.body.password }
     if (userObj == null) return res.send("Please fill all the fields.")
 
     db.db.collection("userInfo").insertOne(userObj);
-    res.statusMessage="Registration Successful"
+    res.statusMessage = "Registration Successful"
     return res.status(200).end()
 })
 
